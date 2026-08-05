59cbeb9b9b0d24449a67da4256f29a6900e60aaf
Tim Stadler <timstadler905@googlemail.com>
2026-08-05 08:56:17 +0200

    fix a weird webassembly error

--- CommonVisionNodesUI/Platforms/WebAssembly/WasmScripts/AppManifest.js
+++ CommonVisionNodesUI/Platforms/WebAssembly/WasmScripts/AppManifest.js
@@ -1,3 +1,9 @@
 var UnoAppManifest = {
-    displayName: "UnoApp1"
+    displayName: "Common Vision Nodes",
+    splashScreenImage: "Assets/Splash/splash_screen.png",
+    splashScreenColor: "transparent",
+    lightThemeBackgroundColor: "#F7F5FC",
+    darkThemeBackgroundColor: "#1C1B1F",
+    accentColor: "#5946D2",
+    darkThemeAccentColor: "#C7BFFF"
 }
